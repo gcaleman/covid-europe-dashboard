49bd662505e1ac2cb04ad640a55ed594d544d62a
Guilherme <guilherme.caleman@gmail.com>
2021-05-27 14:39:20 +0100

    navbar color

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Container, Navbar, Row, Col } from 'react-bootstrap';
+import './NavigationBar.css';
 
 export const NavigationBar = () => (
-  <Navbar bg="light" variant="dark" sticky="top">
+  <Navbar className="navbar" sticky="top">
     <Container>
       <Row>
-        <Navbar.Brand href="/" style={{ color: 'black', textAlign: 'center' }}><h3> COVID DATA CHARTS</h3></Navbar.Brand>
+        <Navbar.Brand href="/" style={{ color: '#fff', textAlign: 'center' }}><h3> COVID DATA CHARTS</h3></Navbar.Brand>
       </Row>
     </Container>
   </Navbar>
