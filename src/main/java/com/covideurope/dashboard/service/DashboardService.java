5626dfc07653ee303999d63aa4fd61ce7a47c4cf
Guilherme <guilherme.caleman@gmail.com>
2021-06-02 12:15:21 +0100

    date gap

--- src/main/java/com/covideurope/dashboard/service/DashboardService.java
+++ src/main/java/com/covideurope/dashboard/service/DashboardService.java
@@ -50,7 +50,7 @@ public class DashboardService {
         List<Covid> dataFromAllOnDates = new ArrayList<>();
         List<LocalDate> listOfDates = getListOfDates();
 
-        for (int i = 0; i < listOfDates.size() - 2; i++) {
+        for (int i = 0; i < listOfDates.size() - 2; i = i + 3) {
             Integer newCases = covidRepository.getSumOfNewCasesAllCountries(listOfDates.get(i));
             Integer totalCases = covidRepository.getSumOfTotalCasesAllCountries(listOfDates.get(i));
             Integer totalDeaths = covidRepository.getSumOfTotalDeathsAllCountries(listOfDates.get(i));
